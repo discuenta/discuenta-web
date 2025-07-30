401ba0edc958f9b64f76766c5bd25692baa261e7
Salim Ramirez <salimramirezm@gmail.com>
2025-07-30 02:16:24 -0500

    feat(i18n): use saved locale from localStorage for i18n setup

--- src/i18n.js
+++ src/i18n.js
@@ -2,8 +2,11 @@ import en from './locales/en.json';
 import es from './locales/es.json';
 import {createI18n} from 'vue-i18n';
 
+const savedLocale = localStorage.getItem('locale') || 'en';
+
 const i18n = createI18n({
-    locale: 'en',
+    legacy: false,
+    locale: savedLocale,
     fallbackLocale: 'en',
     globalInjection: true,
     messages: { en, es }
