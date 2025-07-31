2853e8f68b5c96d5eab43dc703b5fa3fe68adbdc
Salim Ramirez <salimramirezm@gmail.com>
2025-07-30 22:33:45 -0500

    feat: enhance ESLint configuration for Vue files with additional rules and module support

--- eslint.config.js
+++ eslint.config.js
@@ -6,12 +6,29 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,vue}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser }
+    languageOptions: {
+      sourceType: "module",
+      globals: {
+        ...globals.browser
+      },
+    }
   },
+  js.configs.recommended,
+  ...pluginVue.configs["flat/recommended"],
   {
     files: ["**/*.vue"],
-    ...pluginVue.configs["flat/recommended"]
+    rules: {
+      "vue/multi-word-component-names": "off",
+      "vue/require-default-prop": "off",
+
+      "no-console": "warn",
+      "no-debugger": "error",
+      "prefer-const": "error",
+      "no-var": "error",
+      "no-unused-vars": "error",
+
+      "semi": ["error", "always"],
+      "quotes": ["error", "single"]
+    }
   },
 ]);
