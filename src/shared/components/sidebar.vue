1cf29b680008f51a3965f87ec3b4562be44a3a8c
Salim Ramirez <salimramirezm@gmail.com>
2025-07-31 18:02:22 -0500

    feat: add Sidebar component for mobile navigation

---
+++ src/shared/components/sidebar.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, onMounted, onUnmounted, computed } from 'vue';
+import Menu from './menu.vue';
+
+const isMobile = ref(false);
+const visible = ref(false);
+const expanded = ref(true);
+let mediaQuery;
+
+function open() {
+  visible.value = true;
+}
+
+defineExpose({ open });
+</script>
+
+<template>
+  <pv-drawer v-model:visible="visible" position="left" modal dismissable>
+    <menu />
+    <p>Hola</p>
+  </pv-drawer>
+</template>
+
+<style scoped></style>
