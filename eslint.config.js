8180f4ce58668c1369dc5fdcff80a5a597a0fe7f
Salim Ramirez <salimramirezm@gmail.com>
2025-07-30 23:07:56 -0500

    feat: integrate Prettier configuration into ESLint setup for consistent code formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginVue from "eslint-plugin-vue";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
@@ -31,4 +32,5 @@ export default defineConfig([
       "quotes": ["error", "single"]
     }
   },
+  eslintConfigPrettier
 ]);
