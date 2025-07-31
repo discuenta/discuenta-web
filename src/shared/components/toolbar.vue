89ccee845482e406547120e98cd2e3056a61f4d6
Salim Ramirez <salimramirezm@gmail.com>
2025-07-31 18:15:43 -0500

    feat: add Toolbar component for dashboard with sidebar toggle

---
+++ src/shared/components/toolbar.vue
@@ -0,0 +1,23 @@
+<script setup>
+const emit = defineEmits(['toggle-sidebar']);
+</script>
+
+<template>
+  <pv-toolbar>
+    <template #start>
+      <pv-button @click="emit('toggle-sidebar')">=</pv-button>
+      <p>Este es el dashboard</p>
+    </template>
+
+    <template #end>
+      <div>
+        <pv-avatar
+          image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
+          style="width: 32px; height: 32px"
+        />
+      </div>
+    </template>
+  </pv-toolbar>
+</template>
+
+<style scoped></style>
