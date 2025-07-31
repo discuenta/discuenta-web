584406d5160be2664926532e8656421aea9cffdb
Salim Ramirez <salimramirezm@gmail.com>
2025-07-31 02:06:08 -0500

    feat: add ToggleSwitch component and support for dark mode in PrimeVue configuration

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './app.vue';
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
 import 'primeicons/primeicons.css';
-import { SelectButton, Button } from 'primevue';
+import { SelectButton, Button, ToggleSwitch } from 'primevue';
 import i18n from './i18n.js';
 import router from './router/index.js';
 
@@ -16,11 +16,13 @@ createApp(App)
       preset: Aura,
       options: {
         prefix: 'p',
+        darkModeSelector: '.app-dark',
       },
     },
   })
   .component('pv-select-button', SelectButton)
   .component('pv-button', Button)
+  .component('pv-toggle-switch', ToggleSwitch)
   .use(i18n)
   .use(router)
   .mount('#app');
